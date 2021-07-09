c2f5ef497b662ae4666bb965ebbc005432c87eea
Mats Törnberg <mats.tornberg@tretton37.com>
2021-07-09 11:44:44 +0200

    more network

--- client/player.js
+++ client/player.js
@@ -170,7 +170,6 @@ export class Player {
     }
 
     update(data) {
-        console.log(data);
         this.motion = data.metrics.m;
         this.cannonMotion = data.metrics.c;
     }
--- server/server.js
+++ server/server.js
@@ -6,10 +6,20 @@ const wss = new WebSocket.Server({
 wss.on('connection', function connection(ws) {
     ws.hasMoved = false;
 
+    wss.clients.forEach(function each(c) {
+        if (c !== ws && c.lastMetric) {
+            const msg = JSON.parse(c.lastMetric);
+            ws.send(JSON.stringify({ playerConnected: msg.uid, ...msg }));
+        }
+    });
+
     ws.on('message', function incoming(message) {
+        ws.lastMetric = message;
         if (!ws.hasMoved) {
             ws.hasMoved = true;
+            
             const msg = JSON.parse(message);
+            
             wss.clients.forEach(function each(c) {
                 if (c !== ws) {
                     c.send(JSON.stringify({ playerConnected: msg.uid, ...msg }));
